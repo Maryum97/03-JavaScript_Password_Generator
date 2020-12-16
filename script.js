d6883b6051c3ced77a596cc64491be5077581f9d
Maryum Bokhari <maryumbokhari97@gmail.com>
2020-12-16 14:20:46 +0000

    created heading using DOM method

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create all necessary elements
+var h1 = document.createElement("h1");
+
+// Set text content of various elements
+h1.innerHTML = "Password Generator";
+
+// Append elements
+document.body.appendChild(h1);
+
+// Style elements
+h1.setAttribute("style", "margin:auto; width:50%; text-align:center;");
