40709e89b643436953a5b4799053653a6509f5d3
Maryum Bokhari <maryumbokhari97@gmail.com>
2020-12-17 16:45:07 +0000

    updated script

--- scrpit.js
+++ scrpit.js
@@ -25,10 +25,36 @@ function buttonClicked(ev) {
         alert("Your password length must be between 8 and 128 characters.");
     }
     
-    // Asking for lower case
+    // Asking for special characters
     else {
-        confirm("Would you like your password to have lower case characters?")
-
+        confirm("Would you like your password to have special characters?");
 
     }
-}    
+    }    
+
+    // Generating password
+
+    function generatePassword() {
+        //evaluate character type
+        var charSet = "";
+        if( charType.toLowerCase === "lowercase" ) {
+          charSet = "abcdefghijklmnopqrstuvwxyz";
+        } else if( charType.toLowerCase === "uppercase" ) {
+          charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        } else if( charType.toLowerCase === "numeric" ) {
+          charSet = "0123456789";
+        } else if( charType.toLowerCase === "special" ) {
+          charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        } 
+        
+        //return value
+        var retVal = "";
+        //for (var i = 0, n = charSet.length; i < length; i++) {
+          for (var i = 0, n = length; i < length; i++) {
+          //picks a character within charSet at index of random number
+          retVal += charSet.charAt(Math.floor(Math.random() * n));
+        }
+        console.log(retVal);
+        return retVal;
+      }
+
