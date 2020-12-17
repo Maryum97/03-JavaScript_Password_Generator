89941dc0fe2b412645c9babeb9bb475cfde3ef33
Maryum Bokhari <maryumbokhari97@gmail.com>
2020-12-17 15:34:47 +0000

    added character length prompt on script

--- scrpit.js
+++ scrpit.js
@@ -4,26 +4,31 @@ let generateBtn = document.getElementById("generate");
 // Adding event to the button element
 generateBtn.addEventListener('click', buttonClicked);
 
-// Defining the popup messages;
-// var prompt1 = prompt("How many characters do you want your password to be?");
-// var confirm1 = confirm("Would you like your password to have lower case characters?");
-// var confirm2 = confirm("Would you like your password to have upper case characters?");
-// var confirm3 = confirm("Would you like your password to have numeric characters?");
-// var confirm4 = confirm("Would you like your password to have special characters?");
-
-// Defining the Characters
-var length = (len)?(len):(10);
-var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-var numeric = '0123456789';
-var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-var password = " ";
-var character = " ";
-var crunch = true;
+//Asking for Character types:
+// var confirmLowerCase = confirm("Would you like your password to have lower case characters?");
+// var confirmUpperCase = confirm("Would you like your password to have upper case characters?");
+// var confirmNumeric = confirm("Would you like your password to have numeric characters?");
+// var confirmSpecial = confirm("Would you like your password to have special characters?");
 
 // Defining the function
 function buttonClicked(ev) {
     console.log(ev.type, ev.target, ev.currentTarget);
-    prompt("How many characters do you want your password to be?");
+    
+    // Asking for character length
+    var number = prompt("How many characters do you want your password to be?")
 
-    if 
+    if (number < 8) {
+        alert("Your password length must be between 8 and 128 characters.");
+    }
+
+    else if (number > 128) {
+        alert("Your password length must be between 8 and 128 characters.");
+    }
+    
+    // Asking for lower case
+    else {
+        confirm("Would you like your password to have lower case characters?")
+
+
+    }
 }    
